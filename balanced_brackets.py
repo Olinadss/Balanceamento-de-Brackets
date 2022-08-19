87c14be1aee8c81fe3bb176c50ce4080d6274e04
Danilo <danilo.santossouza@outlook.com>
2022-08-19 19:01:54 -0300

    feat: Implementa solução 'Balanceamento de Brackets'

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -4,8 +4,33 @@
 #
 # The function is expected to return a STRING.
 # The function accepts STRING brackets as parameter.
+
+# brackets = "{[(])}"
+
+
 def isBalanced(brackets):
-    # Write your code here
+    if len(brackets) % 2 != 0:
+        return "NO"
+
+    stack_bracket = list()
+    indice = len(stack_bracket) - 1
+
+    for bracket in brackets:
+        if bracket in "([{":
+            stack_bracket.append(bracket)
+        elif bracket == ")" and stack_bracket[indice] == "(":
+            stack_bracket.pop()
+        elif bracket == "]" and stack_bracket[indice] == "[":
+            stack_bracket.pop()
+        elif bracket == "}" and stack_bracket[indice] == "{":
+            stack_bracket.pop()
+        else:
+            return "NO"
+
+    return "YES"
+
+
+# print(isBalanced(brackets))
 
 
 if __name__ == '__main__':
